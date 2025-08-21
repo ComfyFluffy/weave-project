45447251391ae6a214286ab31b9f18831212e2bb
youcht <youcht@liandisys.com.cn>
2025-08-21 15:36:53 +0800

    Introduce JWT authentication mechanism & modify database service

---
+++ apps/backend/src/utils/jwt.ts
@@ -0,0 +1,11 @@
+import { JwtPayload } from '@weave/types/apis'
+import { sign } from 'jsonwebtoken'
+
+export const JWT_SECRET = process.env.JWT_SECRET || 'weave-default-secret-key'
+
+export const generateToken = (payload: JwtPayload): string => {
+  return sign(payload, JWT_SECRET, {
+    expiresIn: '1h',
+    algorithm: 'HS256',
+  })
+}
