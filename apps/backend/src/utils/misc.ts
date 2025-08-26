2799a784353395bb6d7357a3fc628ff9ddd04ef6
youcht <youcht@liandisys.com.cn>
2025-08-26 17:40:58 +0800

    fix-character-creation-bug

--- apps/backend/src/utils/misc.ts
+++ apps/backend/src/utils/misc.ts
@@ -1,4 +1,4 @@
-import { WorldState } from '@weave/types'
+import { WorldState, CharacterState } from '@weave/types'
 
 export const defaultWorldState: () => WorldState['state'] = () => ({
   keyEventsLog: [],
@@ -9,4 +9,26 @@ export const defaultWorldState: () => WorldState['state'] = () => ({
   plots: [],
   lore: [],
   currentGameTime: '',
+})
+
+export const defaultCharacterState: () => CharacterState = () => ({
+  currentLocationName: '',
+  inventory: [],
+  stats: {
+    health: { current: 100, max: 100 },
+    mana: { current: 50, max: 50 },
+  },
+  attributes: {
+    strength: 10,
+    dexterity: 10,
+    constitution: 10,
+    intelligence: 10,
+    wisdom: 10,
+    charisma: 10,
+  },
+  properties: {},
+  knowledge: {},
+  goals: {},
+  secrets: {},
+  discoveredLores: [],
 })
